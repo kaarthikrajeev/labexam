4a5f7ca03d3abcae811ee146ea0e0d52c366f184
kaarthik rajeev <karthick77r@gmail.com>
2022-05-06 16:00:50 +0530

    1st commit

---
+++ arith.sh
@@ -0,0 +1,12 @@
+echo "enter no"
+read a
+echo "enter no"
+read b
+s=`expr $a + $b`
+echo "sum is $s"
+d=`expr $a - $b`
+echo "differnce is $d"
+c=`expr $a \* $b`
+echo "product is $c"
+f=`expr $a / $b`
+echo "quotient is $f"
---
+++ mul.sh
@@ -0,0 +1,7 @@
+echo "enter no"
+read n
+for (( i=1; i<=10; i++))
+do
+m=`expr $i \* $n`
+echo $i "*" $n "=" $m
+done
